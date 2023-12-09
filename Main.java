b2fefca586aa925ed6791fb2732cf45b4bf763b6
ManAnukul <duagjampa@gmail.com>
2023-12-09 14:54:54 +0700

    Test

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ import src.GraphicsSwing;
 public class Main{
     public static void main(String[] args){
         JFrame mainframe = new JFrame();
+        mainframe.add(new GraphicsSwing());
         mainframe.setSize(600,600);
         mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainframe.setVisible(true);
-        mainframe.add(new GraphicsSwing());
     }
 }
