b2408d793f7aeb11e405c0ff47bddb01425db8ab
Pasit Malarat <pasit1006@gmail.com>
2023-12-09 15:14:56 +0700

    black

--- src/GraphicsSwing.java
+++ src/GraphicsSwing.java
@@ -5,6 +5,9 @@ import java.awt.*;
 
 public class GraphicsSwing extends JPanel{
     public void paintComponent(Graphics g){
-
+        g.setColor(Color.BLACK);
+        for(int i = 0;i < 300;i++){
+            g.drawLine(i,0,i,600);
+        }
     }
 }
