74aa84ff156b2c7749ea8a6743a7eb546f4aee6d
ManAnukul <duagjampa@gmail.com>
2023-12-16 17:13:52 +0700

    house2

--- src/GraphicsSwing.java
+++ src/GraphicsSwing.java
@@ -16,7 +16,7 @@ public class GraphicsSwing extends JPanel {
         UtilityFunction.drawLine(33, 448, 33, 509, 1);
         UtilityFunction.drawLine(33, 448, 51, 431, 1);
         UtilityFunction.drawLine(51, 431, 24, 431, 1);
-        UtilityFunction.drawLine(51, 431, 51, 485, 1);
+        UtilityFunction.drawLine(51, 431, 51, 486, 1);
         UtilityFunction.drawLine(0, 448, 24, 431, 1);
 
         UtilityFunction.drawLine(33, 508, 66, 469, 1);
@@ -33,6 +33,35 @@ public class GraphicsSwing extends JPanel {
         UtilityFunction.drawLine(114, 503, 114, 495, 1);
         UtilityFunction.drawLine(114, 503, 108, 503, 1);
         UtilityFunction.drawLine(108, 495, 108, 503, 1);
+
+        UtilityFunction.drawLine(51, 441, 55, 441, 1);
+        UtilityFunction.drawLine(55, 441, 55, 482, 1);
+        UtilityFunction.drawLine(55, 441, 75, 434, 1);
+        UtilityFunction.drawLine(75, 434, 92, 441, 1);
+        UtilityFunction.drawLine(92, 441, 92,469, 1);
+        UtilityFunction.drawLine(55, 447, 75, 439, 1);
+        UtilityFunction.drawLine(75, 439, 92, 448, 1);
+
+        UtilityFunction.drawLine(82, 435, 102, 428, 1);
+        UtilityFunction.drawLine(102, 428, 120, 445, 1);
+        UtilityFunction.drawLine(120, 445, 120, 471, 1);
+
+        UtilityFunction.drawLine(120, 445, 141, 431, 1);
+        UtilityFunction.drawLine(141, 431, 159, 431, 1);
+        UtilityFunction.drawLine(159, 431, 159, 448, 1);
+        UtilityFunction.drawLine(120, 450, 137, 450, 1);
+        UtilityFunction.drawLine(137, 450, 137, 485, 1);
+
+        UtilityFunction.drawLine(154, 486, 154, 494, 1);
+        UtilityFunction.drawLine(154, 494, 157, 494, 1);
+        UtilityFunction.drawLine(157, 494, 157, 485, 1);
+        UtilityFunction.drawLine(157, 485, 167, 485, 1);
+        UtilityFunction.drawLine(167, 485, 168, 465, 1);
+        UtilityFunction.drawLine(168, 465, 157, 446, 1);
+        UtilityFunction.drawLine(157, 446, 144, 465, 1);
+        UtilityFunction.drawLine(144, 465, 144, 479, 1);
+        UtilityFunction.drawLine(144, 479, 147, 484, 1);
+        UtilityFunction.drawLine(147, 484, 154, 484, 1);
 
     }
 
