23d03277b64ba040f7dab034d385105e04bf936c
ManAnukul <duagjampa@gmail.com>
2023-12-18 14:54:35 +0700

    CommentHouse

--- src/GraphicsSwing.java
+++ src/GraphicsSwing.java
@@ -12,6 +12,7 @@ public class GraphicsSwing extends JPanel {
 
 
     private void drawHouse(){
+        //บ้าน 1
         UtilityFunction.drawLine(0, 448, 33, 448, 1);
         UtilityFunction.drawLine(33, 448, 33, 509, 1);
         UtilityFunction.drawLine(33, 448, 51, 431, 1);
@@ -19,6 +20,7 @@ public class GraphicsSwing extends JPanel {
         UtilityFunction.drawLine(51, 431, 51, 486, 1);
         UtilityFunction.drawLine(0, 448, 24, 431, 1);
 
+        //บ้าน 2
         UtilityFunction.drawLine(33, 508, 66, 469, 1);
         UtilityFunction.drawLine(66, 469, 117, 469, 1);
         UtilityFunction.drawLine(0, 448, 24, 431, 1);
@@ -34,6 +36,7 @@ public class GraphicsSwing extends JPanel {
         UtilityFunction.drawLine(114, 503, 108, 503, 1);
         UtilityFunction.drawLine(108, 495, 108, 503, 1);
 
+        //บ้าน 3
         UtilityFunction.drawLine(51, 441, 55, 441, 1);
         UtilityFunction.drawLine(55, 441, 55, 482, 1);
         UtilityFunction.drawLine(55, 441, 75, 434, 1);
@@ -42,16 +45,19 @@ public class GraphicsSwing extends JPanel {
         UtilityFunction.drawLine(55, 447, 75, 439, 1);
         UtilityFunction.drawLine(75, 439, 92, 448, 1);
 
+        //บ้าน 4
         UtilityFunction.drawLine(82, 435, 102, 428, 1);
         UtilityFunction.drawLine(102, 428, 120, 445, 1);
         UtilityFunction.drawLine(120, 445, 120, 471, 1);
 
+        //บ้าน 5
         UtilityFunction.drawLine(120, 445, 141, 431, 1);
         UtilityFunction.drawLine(141, 431, 159, 431, 1);
         UtilityFunction.drawLine(159, 431, 159, 448, 1);
         UtilityFunction.drawLine(120, 450, 137, 450, 1);
         UtilityFunction.drawLine(137, 450, 137, 485, 1);
-
+        
+        //ต้นไม้
         UtilityFunction.drawLine(154, 486, 154, 494, 1);
         UtilityFunction.drawLine(154, 494, 157, 494, 1);
         UtilityFunction.drawLine(157, 494, 157, 485, 1);
