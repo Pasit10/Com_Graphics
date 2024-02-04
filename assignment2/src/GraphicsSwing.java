f1bb8d79866c5e4575c93d30e76db29e293da0b3
Pasit Malarat <pasit1006@gmail.com>
2024-02-04 23:37:01 +0700

    test

--- assignment2/src/GraphicsSwing.java
+++ assignment2/src/GraphicsSwing.java
@@ -6,11 +6,21 @@ import src.UtilityFunction;
 import java.awt.geom.AffineTransform;
 
 public class GraphicsSwing extends JPanel implements Runnable{
-    //double MineMove1 = 
-
     boolean status1 = false;
     boolean status2 = false;
     boolean status3 = false;
+
+    double babyX = 5,babyY = 270;
+
+    double smallcircleX = 170, smallcircleY = 20;
+
+    double midcircleX = 170, midcircleY = 20;
+
+    double circleX = 170;
+    double circleY = 20;
+    double circleHeight = 220;
+    double circleWidth = 350;
+
     @Override public void run(){
         long lastTime = System.currentTimeMillis();
         long startTime = lastTime;
@@ -18,6 +28,7 @@ public class GraphicsSwing extends JPanel implements Runnable{
             long currentTime = System.currentTimeMillis();
             long elapsedTime = currentTime - lastTime;
 
+            //Update
             if (currentTime>=500+startTime) {
                 status1 = true;
             }if (currentTime>=1000+startTime) {
@@ -25,9 +36,21 @@ public class GraphicsSwing extends JPanel implements Runnable{
             }if (currentTime>=1500+startTime) {
                 status3 = true;
             }
-
-            //Update
-
+            // ขยายฝัน
+            double speed = elapsedTime / 1000.0;
+            if((circleX > -200 || circleY > -100) && status3){
+                circleWidth += 300 * speed;
+                circleHeight += 300 * speed;
+                circleX -= 180 * speed;
+                circleY -= 100 * speed;
+            }
+            //ขยับเด็ก
+            if(status3 && midcircleY < getHeight()){
+                babyY += 200 * speed;
+                smallcircleY += 200 * speed;
+                midcircleY += 200 * speed;
+                System.out.println(babyY + " " + smallcircleY + " " + midcircleY);
+            }
             //Display
             repaint();
 
@@ -37,22 +60,23 @@ public class GraphicsSwing extends JPanel implements Runnable{
 
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
+        //clear background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(0,0,getWidth(),getHeight());
+
         UtilityFunction.setupUtilityFunction(g);
-        drawBaby(g2,5,270);
+        drawBaby(g2,(int)babyX,(int)babyY);
         // drawMine(g2, 170,20);
         if (status1) {
-            drawsmallcircle(g2, 170, 20);
+            drawsmallcircle(g2, (int)smallcircleX, (int)smallcircleY);
         }if (status2) {
-            drawmidcircle(g2, 170, 20);
+            drawmidcircle(g2, (int)midcircleX, (int)midcircleY);
         }if (status3) {
-            drawlargecircle(g2, 170, 20);
+            drawlargecircle(g2,(int) circleX, (int)circleY, (int)circleWidth, (int)circleHeight);
         }
     }
 
     public void drawBaby(Graphics2D g2,int x,int y){
-        //head
-        g2.drawOval(x,y,171,167);
-
         //body
         UtilityFunction.drawCurve(x + 30,y + 146 + 40,x + 43,y + 356 + 40,x + 277,y + 321 + 40,x + 164,y + 113 + 40,1);
         g2.drawLine(x + 34,y + 180,x + 54,y + 162);
@@ -64,6 +88,9 @@ public class GraphicsSwing extends JPanel implements Runnable{
         g2.drawOval(x + 70,y + 98,4,4);
         g2.drawOval(x + 110,y + 82,20,20);
         g2.drawOval(x + 120,y + 86,4,4);
+
+        //head
+        g2.drawOval(x,y,171,167);
 
         //แก้ม
         // Rotate ovals by -20 degrees
@@ -95,7 +122,7 @@ public class GraphicsSwing extends JPanel implements Runnable{
         g2.drawOval(x,y + 200,40,20);
     }
 
-    public void drawlargecircle(Graphics2D g2,int x,int y){
-        g2.drawOval(x,y,350,220);
+    public void drawlargecircle(Graphics2D g2,int x,int y,int width,int height){
+        g2.drawOval(x,y,width,height);
     }
 }
