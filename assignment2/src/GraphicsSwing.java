da4086b9f7a55c4eb4945e9253f6bb2743c3e558
Pasit Malarat <pasit1006@gmail.com>
2024-02-04 17:59:17 +0700

    draft1 add หู

--- assignment2/src/GraphicsSwing.java
+++ assignment2/src/GraphicsSwing.java
@@ -19,10 +19,12 @@ public class GraphicsSwing extends JPanel implements Runnable{
 
     public void drawBaby(Graphics g,int x,int y){
         Graphics2D g2 = (Graphics2D)g;
+
+        //head
         g2.drawOval(x,y,171,167);
 
+        //body
         UtilityFunction.drawCurve(x + 30,y + 146 + 40,x + 43,y + 356 + 40,x + 277,y + 321 + 40,x + 164,y + 113 + 40,1);
-
         g2.drawLine(x + 34,y + 180,x + 54,y + 162);
         g2.drawLine(x + 119,y + 161,x + 184,y + 158);
         g2.drawLine(x + 61,y + 243,x + 160,y + 160);
@@ -33,6 +35,7 @@ public class GraphicsSwing extends JPanel implements Runnable{
         g2.drawOval(x + 110,y + 82,20,20);
         g2.drawOval(x + 120,y + 86,4,4);
 
+        //แก้ม
         // Rotate ovals by -20 degrees
         AffineTransform originalTransform = g2.getTransform(); // Save the original transform
 
@@ -46,6 +49,15 @@ public class GraphicsSwing extends JPanel implements Runnable{
         // Restore the original transform
         g2.setTransform(originalTransform);
 
+        //mouth
         UtilityFunction.drawCurve(x + 95, y + 124 + 40, x + 105, y + 126 + 40, x + 107, y + 122 + 40, x + 106, y + 119 + 40, 1);
+
+        //หู
+        // g2.fillRect(x + 7,y + 116,2,2);
+        // g2.fillRect(x + 15,y + 131,2,2);
+        UtilityFunction.drawCurve(x + 7,y + 116 + 40,x ,y + 122 + 40, x + 4,y + 130 + 40,x + 15,y + 131 + 40,1);
+        // g2.fillRect(x + 170,y + 78,2,2);
+        // g2.fillRect(x + 169,y + 94,2,2);
+        UtilityFunction.drawCurve(x + 170,y + 78 + 40,x + 180,y + 80 + 40,x + 178,y + 90 + 40,x + 171,y + 94 + 40,1);
     }
 }
